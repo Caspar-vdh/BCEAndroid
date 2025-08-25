00d253c053ce1467fce601d68a4e5e7f6a2db9b0
CASPARS-SURFACE\c_van <dandykong@gmail.com>
2025-08-25 23:18:54 +0200

    Add background, New game button

--- app/src/main/java/com/dandykong/bceandroid/ui/GameScreenView.kt
+++ app/src/main/java/com/dandykong/bceandroid/ui/GameScreenView.kt
@@ -3,10 +3,16 @@ package com.dandykong.bceandroid.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.dandykong.bceandroid.R
 import com.dandykong.bceandroid.viewmodel.GridViewModel
 
 @Composable
@@ -16,11 +22,20 @@ fun GameScreenView(
 ) {
     Column (
         modifier = modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .paint(
+                painter = painterResource(R.drawable.background),
+                contentScale = ContentScale.FillHeight
+            ),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
 
         ) {
+        Button(
+            onClick = { gridViewModel.resetGame() }
+        ) {
+            Text("New game")
+        }
         GridView(
             cells = gridViewModel.cells,
             onCellClick = gridViewModel::updateCellState
